890eae15a08cc530cc0615d35bf59b649c5fb512
Aligo-tlc <79133939+Aligo-tlc@users.noreply.github.com>
2021-07-22 00:42:51 +0300

    Create password.py

---
+++ password.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+print('Random Password Generator by Aligo using Python')
+length = int(input('\nEnter password legnth'))
+
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+punc = string.punctuation
+
+all = lower + upper + num + punc
+temp = random.sample(all,length)
+password = "".join(temp)
+print(password)
